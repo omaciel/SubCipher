745f6a448ebc4f861be4cbc26eb91ad56e85dc34
Og B. Maciel <omaciel@ogmaciel.com>
2023-07-01 13:05:17 -0400

    Started Zenit Polar cipher.

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func userPrompt() string {
+	fmt.Printf("Message to encrypt:")
+	input, _ := reader.ReadString('\n')
+
+	return strings.TrimSpace(input)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	text := userPrompt()
+
+	for _, letter := range text {
+		switch newLetter := string(letter); newLetter {
+		case "P":
+			fmt.Print("Z")
+		default:
+			fmt.Print(newLetter)
+		}
+
+	}
 }
